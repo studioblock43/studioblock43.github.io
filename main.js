e2d06be534b37d44d31d80dfdce0bf025f2c1760
studioblock43 <54979808+studioblock43@users.noreply.github.com>
2019-09-06 12:42:11 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+// JavaScript Document
+
+//Gallery Navigation-Portfolio Page
+$(function () {	
+		var filterList = {
+			init: function () {
+				// MixItUp plugin
+				// http://mixitup.io
+				$('#portfoliolist').mixItUp({
+  				selectors: {
+    			  target: '.portfolio',
+    			  filter: '.filter'	
+    		  },
+    		  load: {
+      		  filter: '.design'  
+      		}     
+				});								
+			}
+		};
+		
+		// Run the show!
+		filterList.init();
+	});	
+	
+//==== One Page Scroll Down ====
+	
+$('.smooth').on('click', function() {
+    $.smoothScroll({
+        scrollElement: $('body'),
+        scrollTarget: '#' + this.id
+    });
+    
+    return false;
+});
+
+$('a[href*="#"]:not(.carousel-control)').click(function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+});
+
+
+ // ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
